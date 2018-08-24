64f3a414b9f6eb464c418b48596099e6762d6b21
John Agapeyev <jagapeyev@gmail.com>
2018-08-23 17:27:33 -0700

    Moved system call into its own critical section

--- src/belief.cpp
+++ src/belief.cpp
@@ -96,54 +96,57 @@ std::vector<std::vector<bool>> generate_states(const std::vector<std::vector<int
             }
         }
     }
+    std::vector<std::vector<int32_t>> output_states;
 
-    const char* input_filename = ".tmp.input";
-    const char* output_filename = ".tmp.output";
-
-    creat(input_filename, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-    creat(output_filename, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-
+#pragma omp critical(allsat)
     {
-        std::ofstream ofs{input_filename, std::ios_base::out | std::ios_base::trunc};
-        if (!ofs) {
-            std::cerr << "Unable to open output file\n";
+        const char* input_filename = ".tmp.input";
+        const char* output_filename = ".tmp.output";
+
+        creat(input_filename, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+        creat(output_filename, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+
+        {
+            std::ofstream ofs{input_filename, std::ios_base::out | std::ios_base::trunc};
+            if (!ofs) {
+                std::cerr << "Unable to open output file\n";
+                exit(EXIT_FAILURE);
+            }
+
+            for (const auto& clause : clause_list) {
+                std::copy(clause.cbegin(), clause.cend(), std::ostream_iterator<int32_t>(ofs, " "));
+                ofs << "0\n";
+            }
+        }
+
+        system("./minisat_all/bdd_minisat_all_release .tmp.input .tmp.output >/dev/null 2>&1");
+
+        std::ifstream ifs{output_filename};
+        if (!ifs) {
+            std::cerr << "Unable to open results file\n";
             exit(EXIT_FAILURE);
         }
 
-        for (const auto& clause : clause_list) {
-            std::copy(clause.cbegin(), clause.cend(), std::ostream_iterator<int32_t>(ofs, " "));
-            ofs << "0\n";
+        for (std::string line; std::getline(ifs, line);) {
+            std::istringstream iss{std::move(line)};
+
+            std::vector<int32_t> clause_tokens;
+
+            clause_tokens.assign(
+                    std::istream_iterator<int32_t>(iss), std::istream_iterator<int32_t>());
+            clause_tokens.erase(std::remove(clause_tokens.begin(), clause_tokens.end(), 0),
+                    clause_tokens.end());
+            clause_tokens.shrink_to_fit();
+
+            if (clause_tokens.empty()) {
+                continue;
+            }
+
+            output_states.emplace_back(std::move(clause_tokens));
         }
     }
 
-    system("./minisat_all/bdd_minisat_all_release .tmp.input .tmp.output >/dev/null 2>&1");
-
-    std::ifstream ifs{output_filename};
-    if (!ifs) {
-        std::cerr << "Unable to open results file\n";
-        exit(EXIT_FAILURE);
-    }
-
-    std::vector<std::vector<int32_t>> output_states;
-
-    for (std::string line; std::getline(ifs, line);) {
-        std::istringstream iss{std::move(line)};
-
-        std::vector<int32_t> clause_tokens;
-
-        clause_tokens.assign(std::istream_iterator<int32_t>(iss), std::istream_iterator<int32_t>());
-        clause_tokens.erase(
-                std::remove(clause_tokens.begin(), clause_tokens.end(), 0), clause_tokens.end());
-        clause_tokens.shrink_to_fit();
-
-        if (clause_tokens.empty()) {
-            continue;
-        }
-
-        output_states.emplace_back(std::move(clause_tokens));
-    }
-
-    const auto abs_cmp = [](const auto a, const auto b){return std::abs(a) < std::abs(b);};
+    const auto abs_cmp = [](const auto a, const auto b) { return std::abs(a) < std::abs(b); };
 
     for (auto& clause : output_states) {
         std::sort(clause.begin(), clause.end(), abs_cmp);
